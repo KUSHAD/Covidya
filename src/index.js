443fd5f673f11a77543a946cbbd2904aaf836938
KUSHAD <kushad.chakraborty@gmail.com>
2021-08-02 23:45:53 +0530

    some minute changes
    Co-authored-by: RachitCoderAI <RachitCoderAI@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "react-quill/dist/quill.snow.css";
-import "react-virtualized/styles.css";
 import { AuthProvider } from "./context/Auth";
 ReactDOM.render(
 	<React.StrictMode>
